3ef0b4c8e1b7ba9f4c3a11cca0cfa80800e3fc68
Edgar <aedgar1@engr-131-12.eastern.ewu.edu>
2014-02-20 14:49:53 -0800

    1

--- js/scripts.js
+++ js/scripts.js
@@ -3,11 +3,15 @@ $(document).foundation();
 
 // Your Awesome Scripts!
 $(document).ready(function(){
-var msg1 = 'Hello';
-var msg2 = 'Hola';
-var msg3 = 'Bonjour';
-$('h1').text('Hello');
 
-	console.log('Page Loaded. Lets Do this!');
+	var limit = 2;
+	var numRand = Math.floor(Math.random()*limit);
+
+	var msg0 = 'Hello';
+	var msg1 = 'Hola';
+	var msg2 = 'Bonjour';
+	$('h1').text(msg.concat(numRand);
+
+	console.log(msg.concat(numRand);
 
 }); 
